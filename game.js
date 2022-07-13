cf1f9a1a32efaa7377c985e847d65c8e0f39f102
Knowlng <batonelis88@gmail.com>
2022-07-13 21:23:23 +0300

    added "Restart" button

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
 "use strict";
 
-
-
-let randomkvdr= Math.floor(Math.random() * (91-10)+10);
-for (let i=1; i<randomkvdr; i++) {
-    let kvadratelis;
-    let masyvas =[];
-    let br;
-    masyvas.push(i);
-    console.log(masyvas);
-    kvadratelis = document.createElement('div');
-    kvadratelis.classList.add('design');
-    kvadratelis.classList.add('el'+masyvas[0]);
-    document.querySelector('.game').appendChild(kvadratelis);
-    if (i%3==0) {
-        br = document.createElement('br');
-        document.querySelector('.game').appendChild(br);
+function game() {
+    let randomkvdr= Math.floor(Math.random() * (91-10)+10);
+    for (let i=1; i<randomkvdr; i++) {
+        let kvadratelis;
+        let masyvas =[];
+        let br;
+        masyvas.push(i);
+        console.log(masyvas);
+        kvadratelis = document.createElement('div');
+        kvadratelis.classList.add('design');
+        kvadratelis.classList.add('el'+masyvas[0]);
+        document.querySelector('.game').appendChild(kvadratelis);
+        if (i%3==0) {
+            br = document.createElement('br');
+            document.querySelector('.game').appendChild(br);
+        }
     }
-}
 
 
 
@@ -34,11 +33,21 @@ console.log(laimetojas);
 
 
 
-const win = document.querySelectorAll('.el'+laimetojas);
-win.forEach(function(w){
-  w.addEventListener('click', function () {
+document.querySelector('.el'+laimetojas).addEventListener('click', function(){
     this.style.background = "green";
-  });
 });
 
+}
 
+window.onload=game;
+
+document.querySelector('.button').addEventListener('click', function(){
+    const newgame = document.querySelector('.game');
+    newgame.remove();
+    let startnew;
+    startnew = document.createElement('div');
+    startnew.classList.add('game');
+    document.querySelector('.top').appendChild(startnew);
+    game.call(this);
+
+});

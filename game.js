7dea398d57afded856048275eab8beca2e093508
Knowlng <batonelis88@gmail.com>
2022-07-13 17:35:22 +0300

    Initial commit

---
+++ game.js
@@ -0,0 +1,44 @@
+"use strict";
+
+
+
+let randomkvdr= Math.floor(Math.random() * (91-10)+10);
+for (let i=1; i<randomkvdr; i++) {
+    let kvadratelis;
+    let masyvas =[];
+    let br;
+    masyvas.push(i);
+    console.log(masyvas);
+    kvadratelis = document.createElement('div');
+    kvadratelis.classList.add('design');
+    kvadratelis.classList.add('el'+masyvas[0]);
+    document.querySelector('.game').appendChild(kvadratelis);
+    if (i%3==0) {
+        br = document.createElement('br');
+        document.querySelector('.game').appendChild(br);
+    }
+}
+
+
+
+
+const element = document.querySelectorAll('.design');
+element.forEach(function(el){
+  el.addEventListener('click', function () {
+    this.style.background = "red";
+  });
+});
+
+let laimetojas = Math.floor(Math.random() * (randomkvdr-1) +1);
+console.log(laimetojas);
+
+
+
+const win = document.querySelectorAll('.el'+laimetojas);
+win.forEach(function(w){
+  w.addEventListener('click', function () {
+    this.style.background = "green";
+  });
+});
+
+
